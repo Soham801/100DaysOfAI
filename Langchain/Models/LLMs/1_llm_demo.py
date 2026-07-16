c9f088c5940d29c7e45207f3c5506ac63cb7bc07
Soham801 <sohamdeshmukh801@gmail.com>
2026-07-16 16:06:03 +0530

    Integrated Gemini replacing OpenAI in LLM Demo

--- Langchain/Models/LLMs/1_llm_demo.py
+++ Langchain/Models/LLMs/1_llm_demo.py
@@ -1,15 +1,18 @@
 # Import Libraries
 
-from langchain_openai import OpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 # Loaded the OpenAI API Key from .env file
 load_dotenv()
 
 # Created an OpenAI Object as llm
-llm = OpenAI(model = 'gpt-3.5-turbo-instruct')
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
 
 # Using that Object called the invoke function where we sent our prompt and got our response which will be stored in results
 results = llm.invoke("What is the capital of India")
 
-print(results)
+print(results.content)

0d47d1cf0142408abcdef6511a045b5ea18cd385
Soham801 <sohamdeshmukh801@gmail.com>
2026-07-16 22:28:22 +0530

    Studied ChatModels and tested different temp for outputs

---
+++ Langchain/Models/ChatModels/1_chatmodel_openai.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.3
+)
+
+results = model.invoke("What is Neural Networks? Answer in 5 points like a physics teacher who is driving a car")
+
+print(results.content)

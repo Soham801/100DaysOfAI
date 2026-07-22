8abf51ca312e68ed10e1e9737c6a0739969cad76
Soham801 <sohamdeshmukh801@gmail.com>
2026-07-22 20:02:08 +0530

    Started with New Agentic AI Approach

--- Langchain/Models/ChatModels/1_chatmodel_openai.py
+++ Langchain/Models/ChatModels/1_chatmodel_openai.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=0.3
+    temperature=0
 )
 
 results = model.invoke("What is Neural Networks? Answer in 5 points like a physics teacher who is driving a car")

79200d77ca3ec5f1f3150b721ad82c4e9c6990cf
Soham801 <sohamdeshmukh801@gmail.com>
2026-07-22 22:50:53 +0530

    Testing and understanding completion tokens

--- Langchain/Models/ChatModels/1_chatmodel_openai.py
+++ Langchain/Models/ChatModels/1_chatmodel_openai.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    temperature=1.5
+    temperature=1.5,
+    max_completion_tokens=10
 )
 
 results = model.invoke("explain your opinion on recent fifa world cup final in 3 lines")

9903626b400d2134243d5a3072934aaa9875f52e
Soham801 <sohamdeshmukh801@gmail.com>
2026-08-11 18:35:49 +0530

    Learning embedding models for documents

---
+++ Langchain/Models/EmbeddedModels/3_embedding_gemini_document.py
@@ -0,0 +1,18 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2", dimensions=32
+)
+
+documents = [
+    "Delhi is the capital of India"
+    "Mumbai is the capital of Maharashtra"
+    "Banglore is the capital of Karnataka"
+]
+
+results = embeddings.embed_documents(documents)
+
+print(str(results))

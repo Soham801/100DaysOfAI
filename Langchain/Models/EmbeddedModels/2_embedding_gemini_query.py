c5ace46690370bab2cd2182bc890916e430e30c3
Soham801 <sohamdeshmukh801@gmail.com>
2026-08-11 18:25:44 +0530

    Learning embedding models

---
+++ Langchain/Models/EmbeddedModels/2_embedding_gemini_query.py
@@ -0,0 +1,12 @@
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="gemini-embedding-2", dimensions=32
+)
+
+results = embeddings.embed_query("Delhi is the capital of India")
+
+print(str(results))

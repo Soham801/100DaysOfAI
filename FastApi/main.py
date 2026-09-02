ecaa92db08ea5044059bc7c7f3fb0c52eb752dd6
Soham801 <sohamdeshmukh801@gmail.com>
2026-09-02 23:29:34 +0530

    Mentioned server addresss

--- FastApi/main.py
+++ FastApi/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI
 ## To host the server
 app = FastAPI()
 
+## Server 
+
+#http://127.0.0.1:8000/redoc
+#http://127.0.0.1:8000/docs
+
 @app.get("/")
 async def root():
     return {"Message":"Hello World!"}

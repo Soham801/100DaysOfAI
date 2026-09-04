7f1f40270ce7b9e2634bf7f60d2a5271eb8f19d0
Soham801 <sohamdeshmukh801@gmail.com>
2026-09-04 19:51:10 +0530

    learning get requests

--- FastApi/main.py
+++ FastApi/main.py
@@ -11,3 +11,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message":"Hello World!"}
+
+@app.get("/about")
+def about():
+    return {'message': "CampusX is an education platform where you can learn AI"}

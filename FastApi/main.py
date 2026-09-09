a71edfd1f77b7ac3906a1d938717d0a9db255b36
Soham801 <sohamdeshmukh801@gmail.com>
2026-09-09 23:49:14 +0530

    ROUTE WITH MULTIPLE QUERY PARAMETERS

--- FastApi/main.py
+++ FastApi/main.py
@@ -589,3 +589,46 @@ async def async_example():
     return {
         "message": "This is an asynchronous endpoint."
     }
+
+# ============================================================
+# 26. ROUTE WITH MULTIPLE QUERY PARAMETERS
+# ============================================================
+
+@app.get("/calculate")
+def calculate(
+    a: float,
+    b: float,
+    operation: str = "add",
+):
+
+    if operation == "add":
+        result = a + b
+
+    elif operation == "subtract":
+        result = a - b
+
+    elif operation == "multiply":
+        result = a * b
+
+    elif operation == "divide":
+
+        if b == 0:
+            raise HTTPException(
+                status_code=400,
+                detail="Cannot divide by zero",
+            )
+
+        result = a / b
+
+    else:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid operation",
+        )
+
+    return {
+        "a": a,
+        "b": b,
+        "operation": operation,
+        "result": result,
+    }

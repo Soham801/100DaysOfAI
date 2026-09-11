7174a0fe5ea5f15ae6bf7c0b18addb4015e471b8
Soham801 <sohamdeshmukh801@gmail.com>
2026-09-11 23:13:28 +0530

    removes old response chat model

--- FineTuningLLMs/lesson1.py
+++ FineTuningLLMs/lesson1.py
@@ -18,27 +18,9 @@ for prompt in prompts:
                        "role": "user",
                        "content": "Explain Python Dictionaires."
                }
-    ]
+            ]
     )
 
-response = ollama.chat(
-    model="llama3.2",
-    messages= [
-        {
-            "role": "system",
-            "content": '''
-                        You are a Python tutor for beginners.
-                        Always explain concept simply.
-                        Always Provide a small example.
-
-                        '''
-        },
-        {
-            "role": "user",
-            "content": "Explain Python Dictionaires."
-        }
-    ]
-)
 
 print(response["message"]["content"])
 

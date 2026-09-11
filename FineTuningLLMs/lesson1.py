faaff22a99eaebc2629eda4ed43ed17084897f3c
Soham801 <sohamdeshmukh801@gmail.com>
2026-09-11 23:11:30 +0530

    Defined response model

--- FineTuningLLMs/lesson1.py
+++ FineTuningLLMs/lesson1.py
@@ -1,12 +1,20 @@
 import ollama
 
 
-prompt = [
+prompts = [
     "Explain Python dictionaries.",
     "Explain Python dictionaries to a 10-year-old.",
     "Explain Python dictionaries like a university professor.",
     "Explain Python dictionaries using only a code example",
 ]
+
+
+for prompt in prompts:
+
+    response = ollama.chat(
+        model="llama3.2",
+        
+    )
 
 response = ollama.chat(
     model="llama3.2",
